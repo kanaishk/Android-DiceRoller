f00c48d827e7d774c64221bbcc84823dcba4dd38
Kanaishk Garg <kanaishkgarg@gmail.com>
2022-09-16 23:02:33 +0530

    Adding ImageView and setting up its activity

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -22,9 +23,20 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice(luckyNum: Int) {
         val myDice = Dice(6)
+
         val rollResultText: TextView = findViewById(R.id.rollResultText)
         val rollResult:Int = myDice.roll()
         rollResultText.text = rollResult.toString()
+
+        val diceImage: ImageView = findViewById(R.id.diceImage)
+        when(rollResult) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
 
         val toastMsg: String = if(rollResult == luckyNum) {
             "You win!"
