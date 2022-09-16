9a1dadfa6e373b90da48432ac038c61527f69ae1
Kanaishk Garg <kanaishkgarg@gmail.com>
2022-09-16 19:04:52 +0530

    Added diceRoll activity to Button and display value in TextView. Added Dice class

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.diceroller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Roller Button Implementation
+        val rollButton: Button = findViewById(R.id.rollButton)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val myDice = Dice(6)
+        val toastMsg = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+        toastMsg.show()
+        val rollResultText: TextView = findViewById(R.id.rollResultText)
+        rollResultText.text = myDice.roll().toString()
+    }
+}
+
+class Dice(numSides: Int) {
+    private var range:IntRange
+
+    init {
+        range = 1..numSides
+    }
+
+    fun roll(): Int {
+        return range.random()
     }
 }
